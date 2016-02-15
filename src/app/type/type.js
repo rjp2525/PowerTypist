f447bf39b413b8aeb9e946f7b345dce6650db924
bin0 <jeff.allen127@gmail.com>
2016-02-14 19:37:57 -0500

    Lots of changes to timer/starting/stopping game

--- src/app/type/type.js
+++ src/app/type/type.js
@@ -41,21 +41,41 @@ function type() {
     vm.wordsPerMinute = 0;
 
     vm.counter = 60;
+    var timeout = null;
+
+    var timer = false;
 
     function endGame() {
       vm.wordsPerMinute = vm.correctWords.length;
+      $('input').css('background', '#eee');
+      $('input').attr('disabled', '');
+      $('.panel-words').slideUp();
     }
 
-    vm.timer = function() {
-      if (vm.counter != 0) {
-        vm.counter --;
-        timeout = $timeout(vm.timer, 1000);
-      } else {
+    function onTimeout() {
+      if (vm.counter == 0) {
+        $timeout.cancel(timeout);
+        timer = false;
         endGame();
+        return;
+      } else {
+
+        vm.counter -= 1;
+        timer = true;
+        timeout = $timeout(onTimeout, 1000);
       }
     }
 
-    var timeout = $timeout(vm.timer, 1000);
+    function startTimer() {
+      timeout = $timeout(onTimeout, 1000);
+      timer = true;
+    }
+
+    function stopTimer() {
+      vm.counter = 60;
+      $timeout.cancel(timeout);
+      timer = false;
+    }
 
     vm.checkWord = function(event) {
       if (event.keyCode === 32 || event.charCode === 32) {
@@ -78,6 +98,10 @@ function type() {
     }
 
     vm.checkChar = function(event) {
+      if (!timer) {
+        startTimer();
+      }
+
       if (event.keyCode != 32 && event.charCode != 32) {
         if ($('input').val() == vm.wordBank[vm.onWord].substring(0, $('input').val().length)) {
           vm.correctChars += 1;
@@ -91,16 +115,28 @@ function type() {
 
     vm.refresh = function() {
       vm.onWord = 0;
+      vm.onChar = 0;
       vm.correctWords = [];
       vm.incorrectWords = [];
+      vm.correctChars = 0;
+      vm.incorrectChars = 0;
       vm.counter = 60;
+      stopTimer();
       vm.wordsPerMinute = 0;
-      vm.timer;
 
+      $('input').css('background', 'white')
+      $('input').removeAttr('disabled');
       $('input').val('');
       $('input').focus();
       vm.wordBank = WordsService.refresh(vm.wordBank);
+      $('.panel-words').slideDown();
     }
+
+    $(document).on('keydown', function(event) {
+      if (event.which === 8 && !$(event.target).is('input', 'textarea')) {
+        event.preventDefault();
+      }
+    });
   }
 
   angular
